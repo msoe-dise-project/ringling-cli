48b4672831d035cd530d4fbcb941587a8aeddcc0
joshglens <62316290+joshglens@users.noreply.github.com>
2023-06-27 17:02:09 -0500

    Setup.py and README created, basic bin file with argument parser and test script (#2)
    * general setup and readme created, bin file accessible when setup
    
    * address pull request changes
    
    * __init__.py in package, pylint docstring errros
    
    * __init__.py newline

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+Copyright 2023 MSOE DISE Project
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from setuptools import setup
+
+setup(name="ringling-cli",
+      version=0.1,
+      description="Client for Ringling",
+      author="MSOE DISE Project",
+      author_email="nowling@msoe.edu",
+      license="Apache License, Version 2.0",
+      zip_safe=False,
+      packages=["ringling"],
+      python_requires=">=3.8",
+      scripts=["bin/ringling-cli"])
